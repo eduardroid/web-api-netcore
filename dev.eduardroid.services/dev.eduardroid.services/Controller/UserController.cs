342932ec2d5d8f5e8d66f58ee459b09f6491cc7e
@edelcastillo <edelcastillo@xirectss.com>
2020-06-25 21:25:11 -0500

    first commit

---
+++ dev.eduardroid.services/dev.eduardroid.services/Controller/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dev.eduardroid.services.Controller
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome eduardroid");
+        }
+    }
+}
